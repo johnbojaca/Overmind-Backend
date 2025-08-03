ddc61be2058cf0bd44602918fa7d48cdccf0147a
John Bojaca <john.bojaca@gmail.com>
2025-08-03 03:30:24 -0500

    Se quitan los métodos no utilizados

--- src/app.py
+++ src/app.py
@@ -10,30 +10,6 @@ mongo = PyMongo(app)
 
 db = mongo.db
 
-@app.route('/insurance_type', methods=['GET'])
-def getInsuranceTypes():
-    insurance_types = []
-    for type in db.insurance_type.find():
-        insurance_types.append({
-            '_id': str(ObjectId(type['_id'])),
-            'name': type['name'],
-            'image': type['image']
-        })
-    return insurance_types
-
-@app.route('/insurance/<id>', methods=['GET'])
-def getInsurancesByTypeId(id):
-    insurances = []
-    for insurance in db.insurances.find( { "insurance_type": ObjectId(id) } ):
-        insurances.append({
-            '_id': str(ObjectId(insurance['_id'])),
-            'name': insurance['name'],
-            'description': insurance['description'],
-            'benefits': insurance['benefits'],
-            'insurance_type': str(ObjectId(insurance['insurance_type']))
-        })
-    return insurances
-
 @app.route('/insurance', methods=['GET'])
 def getInsurances():
     insurances = []
